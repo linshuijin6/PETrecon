a463405ddd950c5aca21a4f1fe859044cf8965a9
WORK-win <1170414294@qq.com>
2024-09-14 10:27:29 +0800

    Initial commit

---
+++ model/whole_network.py
@@ -0,0 +1,56 @@
+""" 引用函数库 """
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+
+""" **.** 周一 """
+# main_content:
+
+
+def monday():
+
+    return True
+
+
+""" **.** 周二 """
+# main_content:
+
+
+def tuesday():
+
+    return True
+
+
+""" **.** 周三 """
+# main_content:
+
+
+def thursday():
+
+    return True
+
+
+""" **.** 周四 """
+# main_content:
+
+
+def wednesday():
+
+    return True
+
+
+""" **.** 周五 """
+# main_content:
+
+
+def friday():
+
+    return True
+
+
+if __name__ == '__main__':
+    monday()
+    # tuesday()
+    # thursday()
+    # wednesday()
+    # friday()
